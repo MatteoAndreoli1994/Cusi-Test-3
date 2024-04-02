65f71da334043fb16b5427393de7a3e8d5c63562
MatteoAndreoli <matteo.andreoli.business@gmail.com>
2024-04-02 19:41:58 +0200

    commit message

--- src/App.js
+++ src/App.js
@@ -74,7 +74,7 @@ function App() {
 
   return (
     <div className="app">
-
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no"/>
 
 
       <BrowserRouter>
