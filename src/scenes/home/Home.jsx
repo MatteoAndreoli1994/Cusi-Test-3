27e30f31d17bcdcd04a5e7067893abba6a531dad
MatteoAndreoli <matteo.andreoli.business@gmail.com>
2024-04-03 14:38:25 +0200

    commit message

--- src/scenes/home/Home.jsx
+++ src/scenes/home/Home.jsx
@@ -605,7 +605,7 @@ height:auto;
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no"/>
       <Container>
 
-      <GifBox>                 <Video playsInline controls={false} autoPlay loop muted>
+      <GifBox>                 <Video playsInline controls={false} autoPlay loop muted data-setup='{"fluid": true, "bigPlayButton": false}'>
           <source src={video2} type="video/mp4" />
           Your browser does not support the video tag.
         </Video></GifBox>
