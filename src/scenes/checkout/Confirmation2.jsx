8a4b400c702fafc57fa6f497398a57ae810a40a2
MatteoAndreoli <matteo.andreoli.business@gmail.com>
2024-04-26 10:02:52 +0200

    commit message

---
+++ src/scenes/checkout/Confirmation2.jsx
@@ -0,0 +1,31 @@
+import { Box } from "@mui/material";
+import Alert from "@mui/material/Alert";
+import AlertTitle from "@mui/material/AlertTitle";
+import { useDispatch } from 'react-redux';
+import { clearCartAfterConfirmation } from '../../state/index.js';  // Assicurati di importare l'azione corretta
+import styled from 'styled-components';
+
+
+
+
+
+
+
+        
+
+
+const Confirmation = async () => {
+  
+
+  return (
+    <Box m="120px auto" width="80%" height="50vh">
+      <Alert severity="success">
+        <AlertTitle>Success</AlertTitle>
+        You have successfully made an Order —{" "}
+        <strong>Congrats on Making your Purchase</strong>
+      </Alert>
+    </Box>
+  );
+};
+
+export default Confirmation;
