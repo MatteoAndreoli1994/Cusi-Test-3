f51a72267838c710a0df907b184fa1f361682bd1
MatteoAndreoli <matteo.andreoli.business@gmail.com>
2024-05-23 20:56:57 +0200

    commit message

--- src/App.js
+++ src/App.js
@@ -74,6 +74,8 @@ function App() {
     setLoaded(true);
   };
 
+  
+
   return (
     <div className="app">
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no"/>
